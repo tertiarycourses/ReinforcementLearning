ef602fe80519fb2a9933030ef933c3a396981887
Alfred <angch@tertiaryinfotech.com>
2018-03-21 09:34:55 +0800

    0321

--- exercises/module4_1_OpenAI_gym/gym_env_render.py
+++ exercises/module4_1_OpenAI_gym/gym_env_render.py
@@ -1,5 +1,6 @@
 import gym
-env = gym.make('CartPole-v0')
+#env = gym.make('CartPole-v0')
+env = gym.make('FrozenLake-v0')
 # env = gym.make('MountainCar-v0')
 env.reset()
 for _ in range(100):
---
+++ exercises/module4_1_OpenAI_gym/gym_q_table.py
@@ -0,0 +1,37 @@
+import gym
+import numpy as np
+
+env = gym.make('FrozenLake-v0')
+
+#Initialize table with all zeros
+Q = np.zeros([env.observation_space.n,env.action_space.n])
+
+# Set learning parameters
+lr = .8
+y = .95
+num_episodes = 2000
+
+for i in range(num_episodes):
+    #Reset environment and get first new observation
+    s = env.reset()
+    d = False
+    j = 0
+
+    #The Q-Table learning algorithm
+    while j < 99:
+        j+=1
+
+        #Choose an action by greedily (with noise) picking from Q table
+        a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
+
+        #Get new state and reward from environment
+        s1,r,d,_ = env.step(a)
+
+        #Update Q-Table with new knowledge
+        Q[s,a] = Q[s,a] + lr*(r + y*np.max(Q[s1,:]) - Q[s,a])
+        s = s1
+        if d == True:
+            break
+
+print("Final Q-Table Values")
+print(Q)
