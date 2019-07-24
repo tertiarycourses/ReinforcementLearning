e82d955521732b491bbf3389a998a7dfc2911e99
Alfred <angch@tertiaryinfotech.com>
2019-07-24 17:27:18 +0800

    v2

--- exercises/module3_1_OpenAI_gym/gym_env_render.py
+++
@@ -1,9 +0,0 @@
-import gym
-#env = gym.make('CartPole-v0')
-env = gym.make('FrozenLake-v0')
-# env = gym.make('MountainCar-v0')
-env.reset()
-for _ in range(100):
-    env.render()
-    # action = env.action_space.sample()
-    # env.step(action)
